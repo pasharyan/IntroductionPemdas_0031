a1eba60db4954828314b0a00484575b2214231db
pasharyan <pasharyan50@gmail.com>
2026-02-23 12:19:18 +0700

    variabel

---
+++ repos/aaa/app2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+// langkah 1
+int luas,panjang,lebar;
+// langkah 2
+cout << "masukkan panjang :";
+cin >> panjang;
+// langkah 3
+cout << "Masukkan lebar :";
+cin >> lebar;
+// langkah 4
+luas = panjang * lebar;
+// langkah 5
+cout << "Hasilnya : " << luas;
+}
